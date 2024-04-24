511cc8fa3127b66647aa1cb60497c0815d29419f
Max12243 <laurin.maxime1@gmail.com>
2024-04-24 15:42:14 -0400

    retiour max

---
+++ controlleurs/tache.controlleurs.js
@@ -0,0 +1,26 @@
+// CODAGE(controlleurs) DE L'API Tâches
+
+//appel des manipulation de la base de donnée
+const model = require('../models/tache.model.js');
+
+module.exports = {
+
+    afficherTousTaches: (req, res) => {
+        model.obtenirTousTachesDB()
+            .then(result => {
+                res.send(result);
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des tâches :', error);
+                res.status(500).json({ error: 'Erreur serveur' });
+            });
+    }
+
+
+
+
+
+
+
+
+};
---
+++ models/tache.model.js
@@ -0,0 +1,21 @@
+// MANIPULATION(model) DE LA BASE DE DONNÉ POUR L'API Tâches
+
+//Base de donnée
+//const db = require('../.src/config/db.js');
+const db = require("../.src/config/db_pg.js");
+
+//équivalent du main
+module.exports = {
+    obtenirTousTachesDB: () => {
+        return new Promise((resolve, reject) => {    
+            const query = 'SELECT * FROM taches ORDER BY id';
+            db.query(query, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result.rows);
+                }
+            });
+        });
+    },
+};
