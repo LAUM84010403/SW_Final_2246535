40eca0a0b5cd83ba5f31963de1ff87a5fb78bb6c
Max12243 <laurin.maxime1@gmail.com>
2024-05-01 23:43:27 -0400

    Update utilisateur.model.js

--- models/utilisateur.model.js
+++ models/utilisateur.model.js
@@ -1,6 +1,7 @@
 // MANIPULATION(model) DE LA BASE DE DONNÉ POUR L'API Tâches(Utilisateurs)
 
 const bcrypt = require('bcrypt');
+const saltNPepper = 10;
 const uuidv4 = require('uuid');
 //Base de donnée
 const db = require("../.src/config/db_pg.js");
@@ -10,7 +11,7 @@ module.exports = {
     ajouterUtilisateurBD: (req) => {
     return new Promise((resolve, reject) => {
         const cle = uuidv4.v4();
-        const mdpHash = bcrypt.hash(req.body.mot_de_passe)
+        const mdpHash = bcrypt.hash(req.body.mot_de_passe, saltNPepper)
         const query = `INSERT INTO utilisateur(nom, prenom, courriel, cle_api, password) VALUES ($1, $2, $3, $4, $5)`;    
         const params = [req.body.nom, req.body.prenom, req.body.courriel, cle, mdpHash];
         
