1d205a1a07f657a813d5edeea7c06daa8e8eafb8
Max12243 <laurin.maxime1@gmail.com>
2024-05-05 13:13:27 -0400

    mayve

--- .src/models/utilisateur.model.js
+++ .src/models/utilisateur.model.js
@@ -48,7 +48,7 @@ module.exports = {
                     console.log(`Erreur sqlState ${err.sqlState} : ${err.sqlMessage}`);
                     reject(err);
                 }
-                resolve(resultat);
+                resolve(resultat.rows);
             });
         });
     },
