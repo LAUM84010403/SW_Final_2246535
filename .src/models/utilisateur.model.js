6a830d718f64d99fab5d77c28c42046d260499eb
Max12243 <laurin.maxime1@gmail.com>
2024-05-08 20:32:41 -0400

    Update utilisateur.model.js

--- .src/models/utilisateur.model.js
+++ .src/models/utilisateur.model.js
@@ -48,7 +48,7 @@ module.exports = {
                     console.log(`Erreur sqlState ${err.sqlState} : ${err.sqlMessage}`);
                     reject(err);
                 }
-                resolve(resultat.rows[0].id);
+                resolve(resultat.rows.id);
             });
         });
     },
